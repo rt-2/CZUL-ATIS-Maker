9ae5de656c536df8bf9dbb407ad67939fd81769a
rt-2 <rt-2@rt-2.net>
2019-10-26 15:09:53 -0400

    added compatibility to 'VRB' winds

--- AtisMaker/index.php
+++ AtisMaker/index.php
@@ -74,7 +74,7 @@ $metarMainParts = [];
 $metarMainPartStrings = [];
 
 (new MetarMainPart())->SetNew('local', '(?<=^)\w{4}\s\d{2}\d{4}Z(?=\s)');
-(new MetarMainPart())->SetNew('winds', '(?<=\s)\d{5}(?:G\d{2})?KT(?:\s\d{3}V\d{3})?(?=\s)');
+(new MetarMainPart())->SetNew('winds', '(?<=\s)(?>VRB|\d{3})\d{2}(?:G\d{2})?KT(?:\s\d{3}V\d{3})?(?=\s)');
 (new MetarMainPart())->SetNew('visibility', '(?<=\s)(?:\d{1,2}|\d\/\d)SM(?=\s)');
 (new MetarMainPart())->SetNew('precipitations', '(?<=\s)(?:(?:\-|\+)?(?:[A-Z]{2}){1,3}(?=\s)){0,}(?=\s)', false);
 (new MetarMainPart())->SetNew('clouds', '(?<=\s)SKC|(?:\s?(?:FEW|BKN|SCT|OVC)\d{3}){0,}(?=\s)');
