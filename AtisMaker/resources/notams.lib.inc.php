a009b57ed3c90187a237abe8e6799df8edfa8000
rt-2 <rt-2@rt-2.net>
2019-10-27 19:21:46 -0400

    update

--- AtisMaker/resources/notams.lib.inc.php
+++ AtisMaker/resources/notams.lib.inc.php
@@ -1,4 +1,6 @@
 <?php
+
+
 $GLOBALS['notams_array'] = [
 	'CYUL' => [
 		191072 => [
@@ -40,4 +42,11 @@ $GLOBALS['notams_array'] = [
 		],
 	],
 ];
+
+
+
+New NotamTextAdjustments('A' , 'ALPHA');New NotamTextAdjustments('B' , 'BRAVO');New NotamTextAdjustments('C' , 'CHARLIE');New NotamTextAdjustments('D' , 'DELTA');New NotamTextAdjustments('E' , 'ECHO');New NotamTextAdjustments('F' , 'FOXTROT');New NotamTextAdjustments('G' , 'GOLF');New NotamTextAdjustments('H' , 'HOTEL');New NotamTextAdjustments('I' , 'INDIA');New NotamTextAdjustments('J' , 'JULIET');New NotamTextAdjustments('K' , 'KILO');New NotamTextAdjustments('L' , 'LEEMA');New NotamTextAdjustments('M' , 'MICK');New NotamTextAdjustments('N' , 'NOVEMBER');New NotamTextAdjustments('O' , 'OSCAR');New NotamTextAdjustments('P' , 'PAPA');New NotamTextAdjustments('Q' , 'QUEBEC');New NotamTextAdjustments('R' , 'ROMEO');New NotamTextAdjustments('S' , 'SIERRA');New NotamTextAdjustments('T' , 'TANGO');New NotamTextAdjustments('U' , 'UNIFORM');New NotamTextAdjustments('V' , 'VICTOR');New NotamTextAdjustments('W' , 'WHISKEY');New NotamTextAdjustments('X' , 'X-RAY');New NotamTextAdjustments('Y' , 'YANKEE');New NotamTextAdjustments('Z' , 'ZULU');    New NotamTextAdjustments('TWY', 'TAXIWAY');New NotamTextAdjustments('FM', 'FROM');New NotamTextAdjustments('RWY', 'RUNWAY');New NotamTextAdjustments('BTN', 'BETWEEN');New NotamTextAdjustments('AVBL', 'AVAILABLE');New NotamTextAdjustments('CLSD', 'CLOSED');
+
+
+
 ?>
