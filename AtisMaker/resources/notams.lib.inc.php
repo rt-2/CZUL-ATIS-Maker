a9c8df9e6b1346490649631eb242afa7187c7040
rt-2 <rt-2@rt-2.net>
2019-10-28 05:55:44 -0400

    .

--- AtisMaker/resources/notams.lib.inc.php
+++ AtisMaker/resources/notams.lib.inc.php
@@ -5,40 +5,39 @@ $GLOBALS['notams_array'] = [
 	'CYUL' => [
 		191072 => [
 			'en' => 'TWY E BTN RWY 06L/24R AND TWY B CLSD',
-			//'fr' => 'Taxiway '. WrapLetter('E') .' fermÃ© entre la piste 06G/24D et le taxiway '. WrapLetter('B'),
+			'fr' => 'Taxiway E fermé entre la piste 06G/24D et le taxiway B',
 		],
 		191510 => [
 			"en" => 'FIRST 2948FT RWY 06L CLSD',
-			//"fr" => "Premier ".WrapNumberWhole("2948")."ft de la piste 06G fermÃ©",
+			"fr" => "Premier 2948ft de la piste 06G fermé",
 		],
 		191538 => [
 			"en" => "RWY 10/28 CLSD AVBL AS TWY",
-			//"fr" => "Piste 10/28 fermé, disponble comme taxiway",
+			"fr" => "Piste 10/28 fermé, disponble comme taxiway",
 		],
 		[
 			"en" => "TWY G CLSD",
-			//"fr" => "Piste 10/28 fermé, disponble comme taxiway",
+			"fr" => "Taxiway G fermé",
 		]
 	],
-	"CYHU" => [
+	"CYHU" => [ 
 		[
-			"en" => "T",
 			"en" => "TWY B BTN ENA AND 240 FT FM ENA CLSD",
-			//"fr" => "",
+			"fr" => "TWY B ENTRE ENA ET 240 FT DE ENA FERMÉ",
 		],
 		190430 => [
 			"en" => "Runway 10/28 closed, available as taxiway",
-			//"fr" => "Piste 10/28 fermé, disponble comme taxiway",
+			"fr" => "Piste 10/28 fermé, disponble comme taxiway",
 		],
 	],
 	"CYMX" => [
 		190459 => [
 			"en" => "Runway 11/29 closed between sunset and sunrise",
-			//"fr" => "Piste 11/29 fermé entre le crépuscule et soir et le crépuscule du matin",
+			"fr" => "Piste 11/29 fermé entre le crépuscule et soir et le crépuscule du matin",
 		],
 		190465 => [
 			"en" => "Taxiway A closed between taxiway H and holding bay 11",
-			//"fr" => "Taxiway ".WrapLetter("A")." fermé entre le taxiway ".WrapLetter("H")." et l\'air d\'attente piste 11",
+			"fr" => "Taxiway A fermé entre le taxiway H et l\'air d\'attente piste 11",
 		],
 	],
 ];
